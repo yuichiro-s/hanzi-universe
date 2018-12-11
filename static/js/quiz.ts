10bdc1a9c6bb592965ec0e43486386db88e4b754
Yuichiro Sawai <sawai.yuichiro.sn0@is.naist.jp>
2018-12-12 02:15:12 +0900

    Capture keys

--- static/js/quiz.ts
+++ static/js/quiz.ts
@@ -1,21 +1,9 @@
-console.log('hello');
+async function init() {
+    document.addEventListener('keydown', (event) => {
+        const keyName = event.key;
+        console.log(event);
 
-(async function () {
-    let res = await fetch('get_next_question');
-    let {question, answer} = await res.json();
-    console.log(question, answer);
+    }, false);
+}
 
-    let obj = {
-        'question': 'hello',
-        'answer': [2, 3],
-    };
-    let result = await fetch('user_answer',
-        {
-            method: 'POST',
-            body: JSON.stringify(obj),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        },
-    );
-})();
+init();
